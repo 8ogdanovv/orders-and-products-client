df4c37245d5faa364497770fe0188a9c74988a3b
vadym4che <vadym4che@gmail.com>
2023-06-09 18:58:02 +0300

    Deploy to gh-pages

--- src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export const addZero = (digit) => `${String(digit).length === 2 ? digit : '0' + digit}`;
-
-export const formatDate = (dateObj) => {
-  const day = dateObj.getDate();
-  const month = dateObj.toLocaleDateString(undefined, { month: 'short' });
-  const year = dateObj.getFullYear();
-  return `${addZero(day)} ${month}, ${year}`;
-}
